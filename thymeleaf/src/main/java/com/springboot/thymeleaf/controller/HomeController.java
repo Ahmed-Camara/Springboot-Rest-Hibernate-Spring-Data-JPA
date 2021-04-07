e22153ac57ebbd3345acf1c4469f88fbc6aa28c2
Ahmed Camara <44366946+Ahmed-Camara@users.noreply.github.com>
2021-04-07 08:52:11 +0600

    Thymeleaf

---
+++ thymeleaf/src/main/java/com/springboot/thymeleaf/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.springboot.thymeleaf.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	 
+	@GetMapping("/hello")
+	public String showHome(Model model) {
+		 String username = "Ahmed Camara";
+		 
+		model.addAttribute("user",username);
+		
+		return "index";
+	}
+}
